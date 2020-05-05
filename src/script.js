eb50fd125e96be3c49e73f9e28bac57095cea0be
jersh <media2josh@gmail.com>
2020-05-05 16:26:00 -0400

    Updated Linkedin removed src copies of index.html and script.js

--- src/script.js
+++
@@ -1,9 +0,0 @@
-var $body = document.querySelector("body");
-
-document.querySelector(".switch > input").addEventListener('change', function(e){
-  if(e.target.checked){
-    $body.className = "dark";  
-  } else {
-    $body.className = "";
-  }
-});
